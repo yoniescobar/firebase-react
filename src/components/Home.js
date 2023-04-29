506c827322dfd956c4e54d04792bf7582f6fefc6
yoniedilzar <yoniedilzar@gmail.com>
2023-04-29 16:42:18 -0600

    tablero de datos

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,44 @@ const Home = ({correoUsuario}) => {
         <p>Bienvenido, <strong>{correoUsuario}</strong> Haz iniciado Sesión</p>
         <button className='btn btn-danger' onClick={()=>auth.signOut(auth)}>Cerrar Sección</button>
         <hr/>
-        
+
+        <form>
+          <div className="card card-body">
+              <input type="text" placeholder="Ingrese su nombre" className="form-control mb-2" />
+              <input type="text" placeholder="Ingrese su apellido" className="form-control mb-2" />
+              <input type="email" placeholder="Ingrese su correo" className="form-control mb-2" />
+              <input type="text" placeholder="Ingrese su telefono" className="form-control mb-2" />
+             
+          </div>
+
+          <button className="btn btn-primary btn-block mt-2">Registrar</button>
+
+        </form>
+
+         <hr/>
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Correo</th>
+              <th>Telefono</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Carlos</td>
+              <td>Sanchez</td>
+              <td>correo</td>
+              <td>telefono</td>
+              </tr>
+            </tbody>
+              </table> 
+
+
+
+
     </div>
   )
 }
