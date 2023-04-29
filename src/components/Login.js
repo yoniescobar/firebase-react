a00c4221887068cac4fa4723dc3f60173fb0abb9
yoniedilzar <yoniedilzar@gmail.com>
2023-04-29 15:41:55 -0600

    parte visual login

---
+++ src/components/Login.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import img1 from '../img/1.png'
+import img2 from '../img/2.png'
+import img3 from '../img/3.png'
+
+const Login = () => {
+
+    const [registro, setRegistro] = useState(false)
+
+    return (
+        <div className='row'>
+
+            {/* carrusel de imagenes  */}
+            <div className='col-md-8'>
+                <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={img1} className="size-imagen" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src={img2} className="size-imagen" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src={img3} className="size-imagen" alt="..." />
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true" />
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true" />
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+            </div>
+
+            {/* formulario de login */}
+
+            <div className='col-md-4'>
+                <div className='container mt-5'>
+                    <h1 className='text-center'>{registro ? 'Registrate' : 'Iniciar Sesión'}</h1>
+                    <form className='mt-4' >
+                        <div className="mb-3">
+                            <label className='form-label'>Dirección</label>
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder='Ingrese su correo'
+                                name='email'
+                                id='email'
+                                required
+                            />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className='form-label'>Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder='Ingrese su password'
+                                name='password'
+                                id='password'
+                                required
+                            />
+                        </div>
+
+                        <button
+                            type='submit'
+                            className='btn btn-primary mt-4 form-control'
+                        >{registro ? 'Registrarse' : 'Iniciar Sesión'}
+                        </button>
+                    </form>
+
+                    <div className='form-group '>
+                        <button className='btn btn-secondary mt-4 form-control' onClick={() => setRegistro(!registro)}>
+                            {registro ? '¿Ya tienes cuenta?' : '¿No tienes cuenta ? Registrate'}
+                        </button>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Login
---
+++ src/credenciales.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6zl-JcwaBhMWFXxmVi-y8BPGtB-csn3w",
+  authDomain: "app-firebase-b7a8b.firebaseapp.com",
+  projectId: "app-firebase-b7a8b",
+  storageBucket: "app-firebase-b7a8b.appspot.com",
+  messagingSenderId: "762234153544",
+  appId: "1:762234153544:web:e1e93a19401445db62c67d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
